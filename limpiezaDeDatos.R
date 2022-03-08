b56082c7b7bf26bd2cd7650a7c1682f653722dde
Jaime <96883191+jcespinoza2022@users.noreply.github.com>
2022-03-07 23:09:52 -0500

    Add files via upload

---
+++ limpiezaDeDatos.R
@@ -0,0 +1,57 @@
+# Getting and Cleaning Data Project John Hopkins Coursera
+
+# 1. Fusiona los conjuntos de entrenamiento y de prueba para crear un conjunto de datos.
+# 2. Extrae sólo las mediciones sobre la media y la desviación estándar de cada medición.
+# 3. Utiliza nombres de actividad descriptivos para nombrar las actividades del conjunto de datos
+# 4. Etiquete adecuadamente el conjunto de datos con nombres de variables descriptivos.
+# 5. A partir del conjunto de datos del paso 4, crea un segundo conjunto de datos independiente 
+#    y ordenado con la media de cada variable para cada actividad y cada sujeto.
+
+# Cargar paquetes y obtener los datos
+packages <- c("data.table", "reshape2")
+sapply(packages, require, character.only=TRUE, quietly=TRUE)
+path <- getwd()
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url, file.path(path, "dataFiles.zip"))
+unzip(zipfile = "dataFiles.zip")
+
+# Cargar etiquetas de actividad , características
+activityLabels <- fread(file.path(path, "UCI HAR Dataset/activity_labels.txt")
+                        , col.names = c("classLabels", "activityName"))
+features <- fread(file.path(path, "UCI HAR Dataset/features.txt")
+                  , col.names = c("index", "featureNames"))
+featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames])
+measurements <- features[featuresWanted, featureNames]
+measurements <- gsub('[()]', '', measurements)
+
+# Cargar los conjuntos de datos 
+train <- fread(file.path(path, "UCI HAR Dataset/train/X_train.txt"))[, featuresWanted, with = FALSE]
+data.table::setnames(train, colnames(train), measurements)
+trainActivities <- fread(file.path(path, "UCI HAR Dataset/train/Y_train.txt")
+                         , col.names = c("Activity"))
+trainSubjects <- fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt")
+                       , col.names = c("SubjectNum"))
+train <- cbind(trainSubjects, trainActivities, train)
+
+# Conjuntos de datos de pruebas de carga
+test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, featuresWanted, with = FALSE]
+data.table::setnames(test, colnames(test), measurements)
+testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt")
+                        , col.names = c("Activity"))
+testSubjects <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt")
+                      , col.names = c("SubjectNum"))
+test <- cbind(testSubjects, testActivities, test)
+
+# fusionar conjuntos de datos
+combined <- rbind(train, test)
+
+# Convertir las etiquetas de clase en nombre de actividad básicamente. 
+combined[["Activity"]] <- factor(combined[, Activity]
+                                 , levels = activityLabels[["classLabels"]]
+                                 , labels = activityLabels[["activityName"]])
+
+combined[["SubjectNum"]] <- as.factor(combined[, SubjectNum])
+combined <- reshape2::melt(data = combined, id = c("SubjectNum", "Activity"))
+combined <- reshape2::dcast(data = combined, SubjectNum + Activity ~ variable, fun.aggregate = mean)
+
+data.table::fwrite(x = combined, file = "tidyData.txt", quote = FALSE)
